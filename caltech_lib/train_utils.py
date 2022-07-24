Fix fivecrop normalization issues.. among other things

caltech_lib/train_utils.py
@@ -17,19 +17,26 @@ def train_loop(dataloader: DataLoader,
                device: torch.device):
     size = len(dataloader.dataset)
     for batch_num, (images, labels) in enumerate(dataloader):
-        # make prediction and compute loss on this batch
+        curr_batch_size = images.shape[0]
+        n_crops = 1
+        if len(images.shape) == 5:
+            n_crops = images.shape[1]
+            # Using some type of FiveCrop/TenCrop transform - make
+            images = images.view(-1, IMAGE_SIZE[2], IMAGE_SIZE[0], IMAGE_SIZE[1])
         pred = model(images.to(device))
-        loss = loss_function(pred, labels.float().to(device))
 
-        #label_strings = [dataloader.dataset.classnum_to_classname[np.argmax(label.numpy())] for label in labels]
-        #plot_batch(images, label_strings)
-        # Backprop
+        # https://discuss.pytorch.org/t/confused-on-how-to-keep-labels-paired-after-using-five-or-tencrop-augmentation/21289/2
+        pred_avg = pred.view(curr_batch_size, n_crops, -1).mean(1) # avg over crops
+
+        loss = loss_function(pred_avg, labels.float().to(device))
+
         optimizer.zero_grad()
         loss.backward()
         optimizer.step()
 
         loss, current = loss.item(), batch_num * len(images)
         print(f"loss: {loss:>7f}  [{current:>5d}/{size:>5d}]", end='\r')
+
     print("\n")
 
 def test_loop(dataloader: DataLoader,

scripts/train.py
@@ -24,7 +24,7 @@ def main():
     N_CLASSES = 20
     # learning rate
     LEARNING_RATE = 0.0001
-    BATCH_SIZE = 64
+    BATCH_SIZE = 32
     N_EPOCHS = 100
     MODEL_SAVE_FILE = f"/mnt/Terry/ML_models/caltech256_{N_CLASSES}class_classifier.pth"
     # Do transfer learning?
@@ -53,8 +53,13 @@ def main():
                                  transforms.FiveCrop(size=IMAGE_SIZE[:2]),
                                  transforms.Lambda(lambda crops: torch.stack([crop for crop in crops])),
                                  # Normalize images the same way
-                                 transforms.Normalize(mean=[0.485, 0.456, 0.406],
-                                                      std=[0.229, 0.224, 0.225])
+                                 # https://discuss.pytorch.org/t/trouble-using-transforms-fivecrop-tencrop/32059/4
+                                 transforms.Lambda(lambda tensors:
+                                    torch.stack([transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                                                                      std=[0.229, 0.224, 0.225])(t)
+                                                for t in tensors]))
+                                 #transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                                                      #std=[0.229, 0.224, 0.225])
                              ])
 
     filenames = get_dataset_filenames(N_CLASSES, DATADIR)